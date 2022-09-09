7dc0fc54fc09e214012246cdeb7f25dbb0cf188a
Nurudeen010 <nurudeenolamide010@gmail.com>
2022-09-08 21:27:31 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *This programming is use the function put
+ *It explain the power of print
+ *It is a beautiful one and a very good one
+ */
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
