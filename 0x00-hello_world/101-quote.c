1defe079dca51a455dd5bd0f8e599b09f6c7e483
Nurudeen010 <nurudeenolamide010@gmail.com>
2022-09-10 07:45:03 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - This is the function to print integers
+ *
+ * Description: This is the full explanation of the program
+ * Return: The program should return to one succesfully
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
